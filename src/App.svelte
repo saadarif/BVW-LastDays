2ce0f07975dc3a57f6e35b4ecec2787018da9a84
Saad Arif <arifsaad@gmail.com>
2024-05-31 10:28:25 +0100

    mobile testing

--- src/App.svelte
+++ src/App.svelte
@@ -245,7 +245,7 @@
 
 <Header bgfixed={false} theme="light" center={false} short={true}>
 	<h1 style="margin-top: -100px; margin-left: 0px"> The last days of the Black-veined white butterfly in Britain</h1>
-	<div style="margin-top: 5px;  background-color: #fff; opacity: 0.4; z-index: -1;margin-left: 0px"><p class="text-big" >
+	<div style="margin-top: 5px;  background-color: #fff; opacity: 0.6; z-index: -1;margin-left: 0px"><p class="text-big" >
 		<strong>
 			A genomic history of the the Black-veined white butterfly in Britain from its arrival to its demise in the early twentieth century
 		</strong>
